d36222b3b6f1631504469217f6f60c8b2c701cef
Nellyannis Ramirez <nfrv04@gmail.com>
2023-01-19 19:55:21 -0400

    Setting up project basestructure

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:word_app/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:word_app/src/pages/daily_word_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
