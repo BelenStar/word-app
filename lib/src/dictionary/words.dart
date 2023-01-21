a7e283a24900ac9cfa2f93e5831be1d6861304ec
Nellyannis Ramirez <nfrv04@gmail.com>
2023-01-21 18:31:57 -0400

    creating word map

---
+++ lib/src/dictionary/words.dart
@@ -0,0 +1,9 @@
+var words = {
+  "Agibílibus":
+      "Habilidad o ingenio para alcanzar una meta propuesta. Capacidad de manejar las dificultades con un dejo de picardía.",
+  "Petricor":
+      " Nombre que se le otorga al olor que emana la lluvia cuando cae y humedece suelos secos.",
+
+  "Almendruco":
+   "Se le llama asi a la almendra, cuando todavía tiene la cubierta que la recubre verde y su grano aún no cuajó en su totalidad."
+};
